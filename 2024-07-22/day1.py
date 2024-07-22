87cb8c4f1a6e0a03d8e754a9892284585552b2f9
Sam Weissmann <38514250+SPWeissmann@users.noreply.github.com>
2024-07-22 15:46:43 -0400

    Day one files

---
+++ 2024-07-22/day1.py
@@ -0,0 +1,21 @@
+# ctrl-s to save. SAVE OFTEN!!
+# int, integer  - whole numbers
+# str, string   - written text IN QUOTATION MARKS.
+#                 can be letters, numbers, or symbols
+# var, variable - a nickname for some data or information
+# function      - a nickname for some code that does something
+
+x = 10
+y = 5
+z = x + y
+
+print(z)
+
+a = 'Pineapple'
+b = 'pizza'
+c = a + ' ' + b
+
+print(c + ' is delicious')
+
+userIn = input("Please type something: ")
+print("You said: " + userIn)
---
+++ 2024-07-22/madlibs.py
@@ -0,0 +1,18 @@
+#ctrl-s to save. SAVE OFTEN.
+
+celeb = input('Type a celebrity: ')
+person = input('Type a person: ')
+exclam = input('Type an exclamation: ') 
+place1 = input('Type a place: ')
+nounPl = input('Please type a plural noun: ')
+adv1 = input('Type an adverb: ')
+adj1 = input("Type an adjective: ")
+verb1 = input("Type a verb: ")
+verbIng = input('Type a verb that ends in -ing: ')
+weekday = input('Type a day of the week: ')
+
+text = On + ' ' + weekday + ', ' + person + ' and ' + celeb + \
+       + ' were ' + verbIng + ' ' + adv1 + ' at ' + place + \
+       
+
+print(text)
